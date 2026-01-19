168b93c840e75997db1da58b58daf8ed725bfe59
reckerp <git@recker.io>
2026-01-19 09:46:20 +0100

    feat: proper error conversion for status

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -1,9 +1,17 @@
-use crate::git::status;
+use crate::git::{GitError, status};
 use crate::ui::status::render_status;
-use miette::Result;
+use miette::{Diagnostic, Result};
+use thiserror::Error;
+
+#[derive(Error, Debug, Diagnostic)]
+pub enum StatusError {
+    #[error("Could not read git branches")]
+    #[diagnostic(code(gx::git::read_error), help("Are you in a git repository?"))]
+    GitError(#[from] GitError),
+}
 
 pub fn run() -> Result<()> {
-    let status = status::get_repo_status()?;
+    let status = status::get_repo_status().map_err(StatusError::GitError)?;
     render_status(&status);
     Ok(())
 }
